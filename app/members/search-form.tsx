f04ad2718a59d09f802a622837aebe75a8672892
yokishava <aihasayahukoma7@gmail.com>
2023-07-17 11:26:08 +0900

    fix: lang

--- app/members/search-form.tsx
+++ app/members/search-form.tsx
@@ -1,8 +1,35 @@
 import { Grid, Stack, Button, Typography } from "@mui/material";
 import SearchTextInput from "@/app/members/search-text-input";
-import SearchRoleSelectInput from "./search-role-select-input";
-import SearchInterestedActivitySelectInput from "./search-interested-activity-select-input";
-import SearchPMExperienceSelectInput from "./search-pm-experience-select-input";
+import SearchSelectInput from "@/app/members/search-select-input";
+
+const roleSelectInputLabel = "ロール";
+// TODO: fix type
+const roleMenuItems = [
+  "PM",
+  "Engineer",
+  "Marketing",
+  "Sales",
+  "Cutomer Success",
+  "Other",
+];
+
+const pmExperienceSelectInputLabel = "PM歴"
+// TODO: fix type
+const pmExperienceMenuItems = [
+  "PM経験無し",
+  "PMなりたて(1年未満)",
+  "PM歴1~5年",
+  "PM歴5~10年",
+]
+
+const activitySelectInputLabel = "興味ある活動"
+// TODO: fix type
+const activityMenuItems = [
+  "輪読会",
+  "図書館LT",
+  "飲み会/交流",
+  "Shuffle Tea Time",
+]
 
 export default function SearchForm() {
   return (
@@ -12,9 +39,24 @@ export default function SearchForm() {
         属性で絞り込む
       </Typography>
       <Grid container>
-        <SearchRoleSelectInput />
-        <SearchInterestedActivitySelectInput />
-        <SearchPMExperienceSelectInput />
+        <SearchSelectInput
+          props={{
+            menuItems: roleMenuItems,
+            label: roleSelectInputLabel,
+          }}
+        />
+        <SearchSelectInput
+          props={{
+            menuItems: pmExperienceMenuItems,
+            label: pmExperienceSelectInputLabel,
+          }}
+        />
+        <SearchSelectInput
+          props={{
+            menuItems: activityMenuItems,
+            label: activitySelectInputLabel,
+          }}
+        />
       </Grid>
       <Button variant="contained" color="primary">
         検索
